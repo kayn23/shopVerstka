48b27863874454d04fe34c09fe4b5ac89c8fe96d
Anatoly_Chernyshev <kayn23@yandex.ru>
2018-11-08 00:01:46 +0300

    пагинация

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,31 @@
 var mySlider = new Swiper ('.js-slider-top',{
-    speed:400,
-    loop: true
+    speed:800,
+    loop: true,
+    pagination: {
+      el: '.slider--pagination',
+      clickable: true,
+      renderBullet: function (index, className) {
+        return '<span class="' + className + '"></span>';
+      }
+    },
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    }
 });
 
 var mySlider = new Swiper ('.js-slider-bot',{
-  speed:400,
-  loop: true
+  speed:800,
+  loop: true,
+  pagination: {
+    el: '.slider--pagination',
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '"></span>';
+    }
+  },
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  }
 });
